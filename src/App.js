52ea50cedca8488db58382743f7729592c3c6c25
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-12-04 11:15:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
+ 
 import image from './images/image.png';
  
 class App extends React.Component {
