0e9b6b64184cd3ed06754d3182e184d9f19cf833
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-06-06 13:15:35 +0530

    Update CountryPicker.jsx

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -2,7 +2,7 @@
 import { NativeSelect, FormControl } from '@material-ui/core';
 
 import { fetchCountries } from '../../api';
-
+ 
 import styles from './CountryPicker.module.css';
   
 const Countries = ({ handleCountryChange }) => {  
